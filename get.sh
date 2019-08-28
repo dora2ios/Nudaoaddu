da13c665f0d33047a044e7ab1d2457d9506fb66f
dora2-iOS <dora2-iOS@users.noreply.github.com>
2019-08-28 10:48:00 +0900

    Update get.sh

--- get.sh
+++ get.sh
@@ -176,13 +176,13 @@ do
     echo "[SKIP] Already exist shsh2 (apnonce = "$nonce")"
   else
     ./tsschecker -d $1 -B $2 -s -e $ecid -m ""$1"_"$2"_12.4.plist" --save-path shsh2/ --apnonce $nonce 2>/dev/null >/dev/null
+    if [ -e "shsh2/"$ecid"_"$1"_"$2"_12.4-16G77_"$nonce".shsh2" ]; then
+      echo "[SUCCESS] saved apnonce = "$nonce""
+    else
+      echo "[FAILED] not saved apnonce = "$nonce""
+    fi
   fi
 
-  if [ -e "shsh2/"$ecid"_"$1"_"$2"_12.4-16G77_"$nonce".shsh2" ]; then
-    echo "[SUCCESS] saved apnonce = "$nonce""
-  else
-    echo "[FAILED] not saved apnonce = "$nonce""
-  fi
 done
 
 ./tsschecker -d $1 -B $2 -s -e $ecid -m ""$1"_"$2"_12.4.plist" --save-path shsh2/ --generator 0x1111111111111111 2>/dev/null >/dev/null
