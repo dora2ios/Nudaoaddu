c899a63f775f374cdb68c7108cedd02635a9c87a
dora2-iOS <dora2-iOS@users.noreply.github.com>
2019-08-28 11:06:02 +0900

    Update recovery.sh

--- recovery.sh
+++ recovery.sh
@@ -130,10 +130,10 @@ reg=$(bin/igetnonce 2>/dev/null | grep Identified | cut -d ' ' -f 7 )
 if [ $reg = $rec ]; then
 echo "Enter Recovery Mode!!"
 bin/irecovery -c "bgcolor 255 0 255" >/dev/null
-fi
 echo ""
 echo "Tickets that your device uses for futurerestore."
 echo "SHSH2: \"shsh2/"$ecid"_"$1"_"$2"_12.4-16G77_"$nonce".shsh2\""
+fi
 
 rm ibss.signed
 rm ibec.signed
